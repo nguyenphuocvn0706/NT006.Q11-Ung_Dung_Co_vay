54f96b5fbbad12adb4a957b71ce97b48d529e0bd
qt-zz <minhquan030126@gmail.com>
2025-11-12 23:51:40 +0700

    Add files via upload

---
+++ Trần Phúc Minh Quân /Thiet_Ke/Achievement.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Co_Vay
+{
+    partial class Reward
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            btn_BackR = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btn_BackR);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(1, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(799, 449);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.achievement;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(799, 449);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btn_BackR
+            // 
+            btn_BackR.AutoSize = true;
+            btn_BackR.FlatAppearance.BorderSize = 0;
+            btn_BackR.FlatStyle = FlatStyle.Flat;
+            btn_BackR.Image = Properties.Resources.back_to_back;
+            btn_BackR.Location = new Point(43, 356);
+            btn_BackR.Name = "btn_BackR";
+            btn_BackR.Size = new Size(206, 90);
+            btn_BackR.TabIndex = 1;
+            btn_BackR.TabStop = false;
+            btn_BackR.UseVisualStyleBackColor = true;
+            // 
+            // Reward
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Reward";
+            Text = "Achievement";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Button btn_BackR;
+    }
+}
